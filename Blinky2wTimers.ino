35f88c26de83cebda2b671f8d8301b50cae92f48
artiles89 <cold182astro@hotmail.com>
2016-04-06 22:11:44 -0400

    Added files via upload

---
+++ Blinky2wTimers.ino
@@ -0,0 +1,13 @@
+#include <TimerOne.h>
+const int led = 13;
+void setup(void) {
+  // put your setup code here, to run once:
+pinMode(led, OUTPUT);
+Timer1.initialize(166667);
+Timer1.pwm(led,1023);
+}
+
+void loop(void) {
+  // put your main code here, to run repeatedly:
+
+}
